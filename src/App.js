a57351f1e469f06171a2a76d01cf31461a4b3105
mhernandez052 <mjhernandez@mail.csuchico.edu>
2020-04-08 03:11:59 -0700

    adjusting structure

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ function App() {
       <header className="App-header">
         <Title />
         <MapPlaceHolder />
-        {/* <img src={lion} className="App-logo" alt="logo" /> */}
         <CarInfo />
-        <p>Hello from Pakistan</p>
       </header>
     </div>
   );
