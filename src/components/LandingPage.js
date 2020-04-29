b2b2ab9aeb5567ea098c5f7d9f00a9adb34763f6
mhernandez052 <mjhernandez@mail.csuchico.edu>
2020-04-29 03:38:31 -0700

    Added Dest and Origin Input

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,65 +1,76 @@
 import React from 'react';
 import './LandingPage.css';
-import PlacesAutocomplete, {
-  geocodeByAddress,
-  getLatLng
-} from "react-places-autocomplete";
 
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import { geocodeByAddress, getLatLng } from 'react-google-places-autocomplete';
+import 'react-google-places-autocomplete/dist/index.min.css';
 
 class LandingPage extends React.Component {
   constructor() {
     super();
     this.state = {
-      address: '',
-      lat: null,
-      lng: null,
+      originAddress: '',
+      originLat: null,
+      originLng: null,
+
+      destAddress: '',
+      destLat: null,
+      destLng: null
     }
   }
-  handleAddress = (event) => {
-    this.setState({ address: event });
+  handleDestAddress = (event) => {
+    this.setState({ destAddress: event });
   }
 
-  handleSelect = async (value) => {
-    const results = await geocodeByAddress(value);
+  handleOriginAddress = (event) => {
+    this.setState({ originAddress: event })
+  }
+
+  handleOriginSelect = async (value) => {
+    const results = await geocodeByAddress(value.description);
     const latLng = await getLatLng(results[0]);
-    // Results holds a bunch of useful data such as address
-    // console.log(results)
-    this.setState({lat:latLng.lat})
-    this.setState({lng:latLng.lng})
+    this.setState({ originLat: latLng.lat })
+    this.setState({ originLng: latLng.lng })
+  }
+
+  handleDestSelect = async (value) => {
+    const results = await geocodeByAddress(value.description);
+    const latLng = await getLatLng(results[0]);
+    this.setState({ destLat: latLng.lat })
+    this.setState({ destLng: latLng.lng })
   }
   render() {
     return (
       <div>
-        <PlacesAutocomplete
-          value={this.state.address}
-          onChange={this.handleAddress}
-          onSelect={this.handleSelect}
-        >
-          {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-            <div>
-              <p>Latitude: {this.state.lat}</p>
-              <p>Longitude: {this.state.lng}</p>
+        <div className="OriginInput">
+          <GooglePlacesAutocomplete
+            value={this.state.originAddress}
+            onSelect={this.handleOriginSelect}
+            onChange={this.handleOriginAddress}
+            placeholder={"Enter Origin"}
+            inputClassName="AutoFillDropDown"
+            suggestionsClassNames={{
+              container: 'SuggestionContainer',
+              suggestion: 'SuggestionIndividual',
+              suggestionActive: 'SuggestionActive'
+            }}
+          />
+        </div>
+        <div className="DestInput">
+          <GooglePlacesAutocomplete
+            value={this.state.DestAddress}
+            onSelect={this.handleDestSelect}
+            onChange={this.handleDestAddress}
+            placeholder={"Enter Destination"}
+            inputClassName="AutoFillDropDown"
+            suggestionsClassNames={{
+              container: 'SuggestionContainer',
+              suggestion: 'SuggestionIndividual',
+              suggestionActive: 'SuggestionActive',
+            }}
+          />
+        </div>
 
-              <input {...getInputProps({ placeholder: "Enter address" })} />
-
-              <div>
-                {loading ? <div>Loading...</div> : null}
-
-                {suggestions.map(suggestion => {
-                  const style = {
-                    backgroundColor: suggestion.active ? "#EC758F" : "#ffff"
-                  };
-
-                  return (
-                    <div className = "AutoFillDropDown" {...getSuggestionItemProps(suggestion, { style })}>
-                      {suggestion.description}
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          )}
-        </PlacesAutocomplete>
       </div>
     )
   }
